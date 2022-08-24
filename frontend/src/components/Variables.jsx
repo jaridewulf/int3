719b63409ca8df46dde7acbcd2ae5a0ff7458474
Jari De Wulf <jari.de.wulf@student.howest.be>
2022-08-24 13:45:47 +0200

    Add Data tab #1 #2

--- frontend/src/components/Variables.jsx
+++ frontend/src/components/Variables.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { themeContext } from "../App";
 import style from "../styles/Variables.module.css";
 
